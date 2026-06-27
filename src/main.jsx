c2428178acbe2bf4f95c4df5ed328c852878a25e
AHM MRK <ahmmrk.3570m@gmail.com>
2026-06-28 00:39:35 +0330

    fix some bugs

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 import App from './App.jsx';
 
@@ -10,8 +10,8 @@ import 'react-loading-skeleton/dist/skeleton.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='/react-ecommerce-project/'>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
